a4c0a819041ef2149220680fa18de3cac8e2b986
Segii Antropov <homoantropos@gmail.com>
2022-02-19 19:34:28 +0200

    Initial Commit

--- app.js
+++ app.js
@@ -1,5 +1,23 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+// const passport = require('passport');
 
+const sequelize = require('./database/sequelize');
+
+// app.use(passport.initialize());
+// require('./middleware/passport')(passport);
+app.use(require('morgan')('dev'));
+app.use('/uploads', express.static('uploads'));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+app.use(require('cors')());
+
+sequelize.sync({alter: true})
+    .then(
+        () => console.log('DataBase connection established successfully.')
+    ).catch(
+    (err)=> console.log(err)
+)
 
 module.exports = app
