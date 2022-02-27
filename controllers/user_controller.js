8975db953183be0438c167fffc753262b309d01f
Segii Antropov <homoantropos@gmail.com>
2022-02-27 16:35:32 +0200

    User update method added to controller

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -3,39 +3,40 @@ const jwt = require('jsonwebtoken');
 const keys = require('../config/keys');
 const ac = require('../middleware/ac');
 const User = require('../models/User');
+const {response} = require("express");
 
 class User_controller {
 
     async registerUser(req, res) {
-            try {
-                const salt = await bcrypt.genSalt(10);
-                let password = await bcrypt.hash(req.body.password, salt);
-                const role = await ac.getRole(req.body.role);
-                if (typeof role === 'string') {
-                    await User.create({
-                        email: req.body.email,
-                        password,
-                        birthday: new Date(req.body.birthday),
-                        role,
-                        surname: req.body.surname,
-                        name: req.body.name,
-                        phoneNumber: req.body.phoneNumber,
-                        profilePictureSrc: req.file ? req.file.path : ''
-                    });
-                    const user = await User.scope('userResponse').findOne({
-                        where: {email: req.body.email}
-                    });
-                    res.status(201).json(user);
-                } else {
-                    res.status(401).json({
-                        message: role.message ? role.message : role
-                    });
-                }
-            } catch (error) {
-                res.status(500).json({
-                    message: error.message ? error.message : error
-                })
+        try {
+            const salt = await bcrypt.genSalt(10);
+            let password = await bcrypt.hash(req.body.password, salt);
+            const role = await ac.getRole(req.body.role);
+            if (typeof role === 'string') {
+                await User.create({
+                    email: req.body.email,
+                    password,
+                    birthday: new Date(req.body.birthday),
+                    role,
+                    surname: req.body.surname,
+                    name: req.body.name,
+                    phoneNumber: req.body.phoneNumber,
+                    profilePictureSrc: req.file ? req.file.path : ''
+                });
+                const user = await User.scope('userResponse').findOne({
+                    where: {email: req.body.email}
+                });
+                res.status(201).json(user);
+            } else {
+                res.status(401).json({
+                    message: role.message ? role.message : role
+                });
             }
+        } catch (error) {
+            res.status(500).json({
+                message: error.message ? error.message : error
+            })
+        }
     }
 
     async login(req, res) {
@@ -74,42 +75,62 @@ class User_controller {
     }
 
     async updateUser(req, res) {
-             try {
-                const salt = await bcrypt.genSalt(10);
-                let password = await bcrypt.hash(req.body.password, salt);
-                const role = await ac.getRole(req.body.role);
-                if (typeof role === 'string') {
-                    await User.update({
-                        email: req.body.email,
-                        password,
-                        birthday: new Date(req.body.birthday),
-                        role,
-                        surname: req.body.surname,
-                        name: req.body.name,
-                        phoneNumber: req.body.phoneNumber,
-                        profilePictureSrc: req.file ? req.file.path : ''
-                    },
-                        {where: {email: req.user.email}});
-                    const user = await User.scope('userResponse').findOne({
-                        where: {email: req.body.email}
-                    });
-                    res.status(200).json({
-                        user
-                    });
+        try {
+            const candidate = await User.findOne({
+                where: {
+                    id: req.params.id
+                }
+            })
+            if (!candidate) {
+                res.status(400).json({
+                    message: 'Користувача з таким ID не знайдено'
+                })
+            } else {
+                const passwordsCompare = await bcrypt.compare(req.body.actualPassword, candidate.password);
+                if (passwordsCompare) {
+                    const salt = await bcrypt.genSalt(10);
+                    let password = await bcrypt.hash(req.body.password, salt);
+                    let role = req.body.role;
+                    if(role !== req.user.role)
+                    role = await ac.getRole(role);
+                    if (typeof role === 'string') {
+                        await User.update({
+                                email: req.body.email,
+                                password,
+                                birthday: new Date(req.body.birthday),
+                                role,
+                                surname: req.body.surname,
+                                name: req.body.name,
+                                phoneNumber: req.body.phoneNumber,
+                                profilePictureSrc: req.file ? req.file.path : ''
+                            },
+                            {where: {email: req.user.email}});
+                        const user = await User.scope('userResponse').findOne({
+                            where: {email: req.body.email}
+                        });
+                        res.status(200).json({
+                            user
+                        });
+                    } else {
+                        res.status(401).json({
+                            message: role.message ? role.message : role
+                        });
+                    }
                 } else {
                     res.status(401).json({
-                        message: role.message ? role.message : role
-                    });
+                        message: 'INVALID_PASSWORD'
+                    })
                 }
-            } catch (error) {
-                res.status(401).json({
-                    message: error.message ? error.message : error
-                });
             }
+        } catch (error) {
+            res.status(401).json({
+                message: error.message ? error.message : error
+            });
+        }
     }
 
     async getAllUsers(req, res) {
-        if(req.user.role === 'superAdmin') {
+        if (req.user.role === 'superAdmin') {
             try {
                 const users = await User.findAll();
                 res.status(201).json(users);
@@ -118,7 +139,7 @@ class User_controller {
                     message: error.message ? error.message : error
                 })
             }
-        }  else {
+        } else {
             res.status(401).json({
                 message: 'Ви не маєте права реєструвати учасників, зверніться до адміністратора сайту.'
             })
@@ -127,12 +148,25 @@ class User_controller {
 
     async getOneUserById(req, res) {
         try {
-            const user = await User.scope('userResponse').findOne({
+            const candidate = await User.scope('userResponse').findOne({
                 where: {
                     id: req.params.id
                 }
             });
-            res.status(201).json(user);
+            if (candidate) {
+                const emailCompare = req.user.email === candidate.email;
+                if (emailCompare) {
+                    res.status(201).json(candidate);
+                } else {
+                    res.status(401).json({
+                        message: 'Ви не маєте права доступу до профілю цього користувача'
+                    })
+                }
+            } else {
+                res.status(401).json({
+                    message: 'INVALID_PASSWORD'
+                })
+            }
         } catch (error) {
             res.status(500).json({
                 message: error.message ? error.message : error
