e591b20f5f30fbf9b6fc2b8e376900e8495c1de2
Segii Antropov <homoantropos@gmail.com>
2022-03-01 21:21:10 +0200

    reset password option finished

--- utils/confirmationCodeProvider.js
+++ utils/confirmationCodeProvider.js
@@ -1,6 +1,3 @@
 module.exports = class ConfirmationCodeProvider {
     static _userConfirmationCode = 'code';
-    set userConfirmationCode(code) {
-        ConfirmationCodeProvider._userConfirmationCode = code;
-    }
 }
