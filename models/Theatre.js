3378dd861a3c7d0b2acb6f0eb6e7e1555f4647ac
Segii Antropov <homoantropos@gmail.com>
2022-03-11 20:16:01 +0200

    Theater model added

---
+++ models/Theatre.js
@@ -0,0 +1,43 @@
+const Sequelize = require('sequelize');
+const sequelize = require('../database/sequelize');
+const Seat = require('./Seat')
+
+const Theatre = sequelize.define(
+    'theatre', {
+        name: {
+            type: Sequelize.STRING,
+            allowNull: false,
+            unique: 'theatre'
+        },
+        country: {
+            type: Sequelize.STRING,
+            allowNull: false,
+            unique: 'theatre'
+        },
+        town: {
+            type: Sequelize.STRING,
+            allowNull: false,
+            unique: 'theatre'
+        },
+        address: {
+            type: Sequelize.STRING,
+            allowNull: false,
+            unique: 'theatre'
+        }
+    }, {
+        freezeTableName: true,
+        timestamps: false
+    }
+)
+
+Theatre.hasMany(Seat);
+Seat.belongsToMany(Theatre, {as: 'Seats', through: 'TheatreSeats'});
+
+Theatre.addScope('theatre', {
+    attributes: {},
+    includes: [
+        {model: Seat}
+    ]
+})
+
+module.exports = Theatre
